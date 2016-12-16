1cbd3a67277271ef9cf0dd54f460e403b66a731f
rightlag <rightlag@gmail.com>
2016-12-16 11:50:55 -0500

    Add .travis.yml for Swagger specification testing
    For each build, Travis will execute the gulp default task specified in
    'gulpfile.js'. This calls the 'validate' method of the
    [swagger-parser](https://www.npmjs.com/package/swagger-parser) module.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var globby = require('globby');
+var gulp = require('gulp');
+var SwaggerParser = require('swagger-parser');
+
+gulp.task('default', function() {
+  globby(['./specifications/**/*.json']).then(function(entries) {
+    for (var i = 0; i < entries.length; i++) {
+      SwaggerParser.validate(entries[i], function(err, data) {
+        if (err) { throw err; }
+      });
+    }
+  });
+});
