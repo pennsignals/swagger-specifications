786f68735325b9e1d7043c8528b4a323366cf9f7
rightlag <rightlag@gmail.com>
2016-12-18 10:19:37 -0500

    Add Dockerfile to Twilio specification for deploy
    Defines a new Docker image from:
    
    https://github.com/swagger-api/swagger-editor/blob/master/Dockerfile
    
    Overwrites the default Swagger schema used by the editor.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var SwaggerParser = require('swagger-parser');
 
 gulp.task('test', function() {
-  globby(['./specifications/**/*.json']).then(function(specifications) {
+  globby(['./specifications/**/*.(json|yml)']).then(function(specifications) {
     specifications.forEach(function(specification) {
       SwaggerParser.validate(specification, function(err, data) {
         if (err) { throw err; }
--- specifications/twilio/README.md
+++ specifications/twilio/README.md
@@ -1,3 +1,5 @@
 # [TWILIO REST API](https://www.twilio.com/docs/api/rest)
 
+[![Deploy to Docker Cloud](https://files.cloud.docker.com/images/deploy-to-dockercloud.svg)](https://cloud.docker.com/stack/deploy/)
+
 The Twilio REST API allows you to query meta-data about your account, phone numbers, calls, text messages, and recordings. You can also do some fancy things like initiate outbound calls and send text messages.
