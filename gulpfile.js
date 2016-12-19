055ea40680dfc788a65f92a352abefaa50eddb8e
rightlag <rightlag@gmail.com>
2016-12-19 11:36:04 -0500

    Update gulpfile.js to include (yml|yaml) files
    The gulp 'test' task was using the pattern matching for globby
    incorrectly. The patterns have been updated to include any pattern
    matching 'swagger.(json|yml|yaml)'.

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,13 @@ var gulp = require('gulp');
 var SwaggerParser = require('swagger-parser');
 
 gulp.task('test', function() {
-  globby(['./specifications/**/*.(json|yml)']).then(function(specifications) {
+  var patterns = ['./specifications/*/swagger.{json,yml,yaml}'];
+  globby(patterns).then(function(specifications) {
     specifications.forEach(function(specification) {
       SwaggerParser.validate(specification, function(err, data) {
         if (err) { throw err; }
+        console.log(specification);
+        console.log(data);
       });
     });
   });
